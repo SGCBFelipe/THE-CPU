443854889f1428abc11574d5540024d06750ef74
Professor Felipe - SuperGeeks Campo Belo <felipe.campobelo@supergeeks.com.br>
2026-03-05 10:47:31 -0300

    menu

--- The CPU/Assets/scripts/GameManager.cs
+++ The CPU/Assets/scripts/GameManager.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using TMPro;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,8 +10,21 @@ public class GameManager : MonoBehaviour
 
     public void AlertMessage(string text)
     {
+        if (message == null)
+            return;
+
         Animator _messageAnimator = message.GetComponent<Animator>();
         message.text = text;
         _messageAnimator.SetTrigger("Message");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("maze");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
